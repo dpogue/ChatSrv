d6c402c2436ada28a74bd13479cc226382f14454
Terence Stenvold <tstenvold@gmail.com>
2011-03-19 18:38:07 -0700

    Now writes to a file as well :D

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -9,55 +9,97 @@
 #include <strings.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
+
 
 #define SERVER_TCP_PORT 6667// Default port
 #define BUFLEN 4096  // Buffer length
 
+void writetoFile(FILE *fp, char *msg, pthread_mutex_t *mtx);
+void openFile(FILE **fp, char *filename);
 void conServer(int* sd, int port, char* host);
 void initChat(char* nick, char* channel, char* server, int sd);
-void recvLoop(int sd);
+void recvLoop(int sd, FILE *fp, int argc, pthread_mutex_t *mtx);
+
 
 int main(int argc, char** argv)
 {
-    int sd, port;
+    int sd, port, child;
     char* host;
     char* bp;
     char sbuf[BUFLEN], tbuf[BUFLEN];
     char nick[BUFLEN];
     char channel[BUFLEN];
-
+    pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+    FILE *fp = NULL;
+    
     switch (argc) {
         case 2:
             host = argv[1]; // Host name
             port = SERVER_TCP_PORT;
             break;
         case 3:
+        case 4:
             host = argv[1];
             port = atoi(argv[2]); // User specified port
             break;
         default:
-            fprintf(stderr, "Usage: %s host [port]\n", argv[0]);
+            fprintf(stderr, "Usage: %s host [port] [filename] \n", argv[0]);
             exit(1);
+    }
+
+    if(argc == 4) {
+	openFile(&fp,argv[3]);
     }
 
     conServer(&sd, port, host);
 
-    if (fork() > 0) {
-        recvLoop(sd);
+    if(child = fork() > 0) {
+        recvLoop(sd,fp,argc,&mtx);
     }
 
     initChat(nick, channel, host, sd);
 
     while (1) {
         fgets(tbuf, BUFLEN, stdin);
-        sprintf(sbuf, "PRIVMSG %s :%s", channel, tbuf);
-        send(sd, sbuf, strlen(sbuf), 0);
+        
+	if(strlen(tbuf) == 3 && strcmp(tbuf,"/q\n") == 0 ) {
+	    printf("\n\nQUITTING!\n");
+	    break;
+	}
+	sprintf(sbuf, "PRIVMSG %s :%s", channel, tbuf);
+	
+ 	if(argc == 4) {
+	    writetoFile(fp,tbuf,&mtx);
+	}
+	send(sd, sbuf, strlen(sbuf), 0);
     }
 
+    kill(child, SIGKILL);
     close(sd);
+    fclose(fp);
     return 0;
 }
 
+void writetoFile(FILE *fp, char *msg, pthread_mutex_t *mtx)
+{
+    pthread_mutex_unlock(mtx);
+    fprintf(fp,"%s",msg);
+    pthread_mutex_unlock(mtx);
+    fflush(fp);
+}
+
+void openFile(FILE **fp, char *filename) 
+{
+    *fp = fopen (filename,"w");
+    if(*fp==NULL)
+    {
+	printf("file open failed\n");
+	exit(1);
+    }
+}
 
 void conServer(int* sd, int port, char* host)
 {
@@ -89,15 +131,16 @@ void conServer(int* sd, int port, char* host)
     }
 }
 
-void recvLoop(int sd)
+void recvLoop(int sd, FILE *fp, int argc, pthread_mutex_t *mtx)
 {
     int n, bytes_to_read;
     char rbuf[BUFLEN];
     char tbuf[8];
     char* bp;
 
-    while (1) {
-        n = 0;
+    while(1) {
+        
+	n = 0;
         bp = rbuf;
         bytes_to_read = BUFLEN;
 
@@ -110,13 +153,16 @@ void recvLoop(int sd)
                 break;
             }
 
-            usleep(1000);
+            usleep(100);
         }
 
         strncpy(tbuf, rbuf, 6);
 
         if (strcmp(tbuf, "NOTICE") != 0) {
-            printf("%s", rbuf);
+	    if(argc == 4) {
+		writetoFile(fp,rbuf,mtx);
+	    }
+	    printf(rbuf);
             fflush(stdout);
         }
     }
